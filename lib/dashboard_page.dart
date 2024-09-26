d807f48e70734cee07ba37603e82737188eb24c3
fxdamassa <afagundes777@gmail.com>
2024-09-26 20:38:07 -0300

    Realizado modificação no dashboard para a função logout() aonde o mesmo não estava pegando o token

--- lib/dashboard_page.dart
+++ lib/dashboard_page.dart
@@ -4,9 +4,9 @@ import 'package:barberapp/schedule_page.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
-import 'main.dart';
 import 'contact_page.dart';
 import 'edit_profile_page.dart';
+import 'agenda_page.dart'; // Certifique-se de que você importou a página da agenda
 
 class DashboardPage extends StatelessWidget {
   // Função para obter o token do SharedPreferences
@@ -15,36 +15,37 @@ class DashboardPage extends StatelessWidget {
     return prefs.getString('auth_token');
   }
 
+  // Função para obter a role do usuário
+  Future<String?> getUserRole() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString('user_role');
+  }
+
   // Função de logout com o context
   Future<void> logout(BuildContext context) async {
     try {
-      final token = await getToken();
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      String? token = prefs.getString('auth_token');
 
-      if (token == null) {
-        print('Usuário não autenticado');
-        return;
-      }
-
-      final response = await http.post(
-        Uri.parse('http://10.0.2.2:8000/api/logout'),
-        headers: {
-          'Authorization': 'Bearer $token',
-        },
-      );
-
-      if (response.statusCode == 200) {
-        print('Logout bem-sucedido');
-
-        final prefs = await SharedPreferences.getInstance();
-        await prefs.remove('auth_token');
-
-        // Redireciona para a tela de login
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (context) => LoginPage()),
+      if (token != null) {
+        final response = await http.post(
+          Uri.parse('http://10.0.2.2:8000/api/logout'),
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer $token', // Usa o token aqui
+          },
         );
-      } else {
-        print('Falha no logout: ${response.body}');
+
+        if (response.statusCode == 200) {
+          await prefs.remove('auth_token');
+          await prefs.remove('user_role');
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => LoginPage()),
+          );
+        } else {
+          print('Falha no logout: ${response.statusCode} - ${response.body}');
+        }
       }
     } catch (e) {
       print('Erro ao fazer logout: $e');
@@ -58,43 +59,65 @@ class DashboardPage extends StatelessWidget {
         title: Text('Dashboard'),
       ),
       drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            ListTile(
-              title: Text('Agendar Corte'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => SchedulePage()),
-                );
-              },
-            ),
-            ListTile(
-              title: Text('Editar Cadastro'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => EditProfilePage()),
-                );
-              },
-            ),
-            ListTile(
-              title: Text('Contato'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ContactPage()),
-                );
-              },
-            ),
-            ListTile(
-              title: Text('Sair'),
-              onTap: () async {
-                await logout(context); // Passa o contexto para a função logout
-              },
-            ),
-          ],
+        child: FutureBuilder<String?>(
+          future: getUserRole(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
+
+            final userRole = snapshot.data;
+
+            return ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                ListTile(
+                  title: Text('Agendar Corte'),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SchedulePage()),
+                    );
+                  },
+                ),
+                ListTile(
+                  title: Text('Editar Cadastro'),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => EditProfilePage()),
+                    );
+                  },
+                ),
+                ListTile(
+                  title: Text('Contato'),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ContactPage()),
+                    );
+                  },
+                ),
+                // Exibe a agenda administrativa somente se o usuário for admin
+                if (userRole == 'admin')
+                  ListTile(
+                    title: Text('Agenda Administrativa'),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => AgendaPage()),
+                      );
+                    },
+                  ),
+                ListTile(
+                  title: Text('Sair'),
+                  onTap: () async {
+                    await logout(context); // Passa o contexto para a função logout
+                  },
+                ),
+              ],
+            );
+          },
         ),
       ),
       body: Center(
